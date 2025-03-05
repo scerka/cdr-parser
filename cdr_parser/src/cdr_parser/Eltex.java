18c04b07fc38fb64ab8b12840b3a7b3f96618a08
Scerka <sceri73@gmail.com>
2025-03-05 14:57:30 +0400

    Изменение Eltex парсера (условия переадресации)

--- cdr_parser/src/cdr_parser/Eltex.java
+++ cdr_parser/src/cdr_parser/Eltex.java
@@ -47,38 +47,58 @@ public class Eltex {
 				 String[] lines = line.split(";");
 				 
 				 String type = "Eltex";
-				 String aNumber = lines[11];
-				 String bNumber = lines[23];
-				 String callStart = convertDate(lines[1]);
-				 String callStop = convertDate(lines[27]);
-				 String callDuration = lines[2];
+				 String aNumber = lines[11]; // Номер А
+				 String bNumber = lines[23]; // Номер Б
+				 String callStart = convertDate(lines[1]); // Дата начала звонка
+				 String callStop = convertDate(lines[27]); // Дата окончания звонка
+				 String callDuration = lines[2]; // Длительность звонка
 				 String code = lines[3] + "/" + lines[4];
-				 String trunkIn = lines[7];
-				 String trunkOut = lines[18];
+				 String trunkIn = lines[7]; // Входящий транк
+				 String trunkOut = lines[18]; // Исходящий транк
 				 String redirFlag = lines[13];
 				 String answerFlag = Integer.parseInt(callDuration) > 0 ? "ANSWER" : "NO_ANSWER";
 				 
+				 // Если номер Б (23) пустой, берём значение из другого "столбца" 22
 				 if (bNumber == "") bNumber = lines[22];
 				 
 				 // if it is internal call, then use another fields for IN/OUT trunks
+				 // если это внутренний вызов, то используйте другие поля для входящих/исходящих транков
 				 String regex = "^\\d+$";
 				 
+				 // Если входящий транк (7) состоит только из цифр, то берём значение из "столбеца" 6
 				 if (trunkIn.matches(regex)) {
 					 trunkIn = lines[6];
 				 }
 				 
+				 // Если исходящий транк (18) состоит только из цифр, то берём значение из "столбеца" 17
 				 if (trunkOut.matches(regex)) {
 					 trunkOut = lines[17];
 				 }
 				 
 				 // it is two cdr lines for redirected call, use another fields for A/B-numbers
+				 // это две строки cdr для перенаправленного вызова, используйте другие поля для номеров A/B
+				 
+				 // Изменения, новые условия
+				 if (redirFlag.matches("redirecting") || redirFlag.matches("redirected")) {
+					 // тут мы знаем что поле с номером A не может быть пустым, проверка не нужна
+					 aNumber = lines[14];
+					 
+					 // Проверка на пустое поле
+					 // В данном случае мы не уверены что "столбец" 22 не пустой, ЕСЛИ ОН ОКАЖЕТСЯ ПУСТ, ТО ЗНАЧЕНИЕ НЕ ИЗМЕНИТЬСЯ И ВОЗЬМЁТЬСЯ С 23
+					 if (lines[22] != "") {
+						 bNumber = lines[22];
+					 }
+				 }
+				 
+				 /* Оригинальные условия парсера eltex_smg_2016
 				 if (redirFlag.matches("redirecting")) {
 					 aNumber = lines[14];
 				 }
-				 
+				  
 				 if (redirFlag.matches("redirected")) {
 					 bNumber = lines[22];
 				 }
+				 */
 				 
 				 printWriter.println(type + ";" + answerFlag + ";" + aNumber + ";" + bNumber + ";" + callStart + ";" + callStop + ";" + callDuration + ";" + trunkIn + ";" + trunkOut + ";" + code + ";" + "");
 				 
